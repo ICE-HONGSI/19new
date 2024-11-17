a8c1b0b8289afe267e138aae62ed05bc3aea4923
SionLee21 <sionl1030@gmail.com>
2024-11-17 14:28:22 +0900

    돌아가는 코드

--- src/main/java/com/ubinetsys/pms/login/LoginController.java
+++ src/main/java/com/ubinetsys/pms/login/LoginController.java
@@ -25,6 +25,7 @@ public class LoginController {
 	private LoginBO loginBO;
 
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	private static final boolean isTestMode = true; // 테스트 모드 설정
 
 	@RequestMapping("/")
 	public String login(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
@@ -32,50 +33,52 @@ public class LoginController {
 		return page;
 	}
 
-	
 	String autoLogin(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
 		boolean autologin = false;
-		
+
 		Cookie cookie_userid = null;
 		Cookie cookie_pwd = null;
 		Cookie cookie_autologin = null;
-		
+
 		Cookie[] cookies = request.getCookies();
 		String userid = null, passwd = null;
-		
+
 		if (cookies != null) {
-		 for (Cookie cookie : cookies) {
-		   if (cookie.getName().equals("userid")) { 
-			   userid = cookie.getValue();
-			   cookie_userid = cookie;
-		   }
-		   else if (cookie.getName().equals("pwd")) {
-			   passwd = cookie.getValue();		   
-			   cookie_pwd = cookie;   
-		   }
-		   else if (cookie.getName().equals("autologin")) {
-			   if(cookie.getValue().isEmpty()) break;			   
-			   else if(cookie.getValue().equals("1")) {
-				   cookie_autologin = cookie;   
-				   autologin = true;
-			   }			   
-		    }
-		  }
-		}	
-		
-		if(autologin) {
-			User userinfo = loginBO.findByUserIdAndPassword(userid, passwd);
-			if(userinfo != null) {
-				
-				//쿠키 유효기간 수정
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("userid")) {
+					userid = cookie.getValue();
+					cookie_userid = cookie;
+				} else if (cookie.getName().equals("pwd")) {
+					passwd = cookie.getValue();
+					cookie_pwd = cookie;
+				} else if (cookie.getName().equals("autologin")) {
+					if (cookie.getValue().isEmpty()) break;
+					else if (cookie.getValue().equals("1")) {
+						cookie_autologin = cookie;
+						autologin = true;
+					}
+				}
+			}
+		}
+
+		if (autologin) {
+			User userinfo;
+			if (isTestMode) {
+				userinfo = createTestUser(); // 가짜 유저 생성
+			} else {
+				userinfo = loginBO.findByUserIdAndPassword(userid, passwd);
+			}
+
+			if (userinfo != null) {
+				// 쿠키 유효기간 수정
 				int expiretime = 60 * 60 * 24 * 30;
 				cookie_userid.setMaxAge(expiretime);
 				cookie_pwd.setMaxAge(expiretime);
 				cookie_autologin.setMaxAge(expiretime);
-				response.addCookie(cookie_userid);	
+				response.addCookie(cookie_userid);
 				response.addCookie(cookie_pwd);
 				response.addCookie(cookie_autologin);
-				
+
 				session.setAttribute("userLoginInfo", userinfo);
 				logger.info("autologin : main page");
 				return "redirect:/main";
@@ -84,37 +87,43 @@ public class LoginController {
 		logger.info("autologin : login page");
 		return "login";
 	}
-	
+
 	@RequestMapping("logout")
 	public String logout(HttpSession session, HttpServletResponse response) {
 		session.invalidate();
-		
-		//delete autologin cookie 
+
+		// delete autologin cookie
 		Cookie cookie_pwd = new Cookie("pwd", null);
-		Cookie cookie_autologin= new Cookie("autologin", null);
-		cookie_pwd.setMaxAge(0);				
-		cookie_autologin.setMaxAge(0); 
-		response.addCookie(cookie_autologin); 		
-		response.addCookie(cookie_pwd); 
-		
+		Cookie cookie_autologin = new Cookie("autologin", null);
+		cookie_pwd.setMaxAge(0);
+		cookie_autologin.setMaxAge(0);
+		response.addCookie(cookie_autologin);
+		response.addCookie(cookie_pwd);
+
 		return "redirect:/";
 	}
 
 	@ResponseBody
 	@RequestMapping(value = "loginProcess", method = RequestMethod.POST)
 	public ModelAndView loginProcess(@RequestParam("Uuid") String userid, @RequestParam("Upwd") String passwd,
-			HttpSession session, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+									 HttpSession session, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
 		logger.info("===> Start loginProcess1");
-		
+
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("redirect:/");
 		if (userid == null || passwd == null)
 			return mav;
-						
+
 		passwd = testMD5(passwd);
-		
-		User userinfo = loginBO.findByUserIdAndPassword(userid, passwd);
+
+		User userinfo;
+		if (isTestMode) {
+			userinfo = createTestUser(); // 가짜 유저 생성
+		} else {
+			userinfo = loginBO.findByUserIdAndPassword(userid, passwd);
+		}
+
 		if (userinfo == null) {
 			logger.info("===> Login fail");
 			mav.addObject("msg", "f");
@@ -122,42 +131,39 @@ public class LoginController {
 			logger.info("===> Login" + userinfo.getGname() + userinfo.getUlevel());
 			session.setAttribute("userLoginInfo", userinfo);
 			mav.addObject("msg", "s");
-			
-			int mode = 0;
-			if(request.getParameter("saveId") != null) mode = 1;
-			if(request.getParameter("saveBoth") != null) mode = 2;
-			setLoginCookie(response, mode, userid, passwd);
-		}		
-		return mav;
 
+			int mode = 0;
+			if (request.getParameter("saveId") != null) mode = 1;
+			if (request.getParameter("saveBoth") != null) mode = 2;
+			setLoginCookie(response, mode, userid, passwd);
+		}
+		return mav;
 	}
-	
-	public void setLoginCookie(HttpServletResponse response,  int mode, String userid, String pwd) {
+
+	public void setLoginCookie(HttpServletResponse response, int mode, String userid, String pwd) {
 		int expiretime = 60 * 60 * 24 * 30;
-		
+
 		logger.info("mode : " + mode);
-		Cookie cookie_id = new Cookie("userid", userid);				
+		Cookie cookie_id = new Cookie("userid", userid);
 		cookie_id.setMaxAge(expiretime);
 		response.addCookie(cookie_id);
-		
+
 		Cookie cookie_pwd = new Cookie("pwd", pwd);
 		Cookie cookie_autologin = new Cookie("autologin", "1");
-		if(mode == 2) {			
-			cookie_autologin.setMaxAge(expiretime);				
+		if (mode == 2) {
+			cookie_autologin.setMaxAge(expiretime);
 			cookie_pwd.setMaxAge(expiretime);
-		}
-		else { 			
+		} else {
 			cookie_autologin.setMaxAge(0);
 			cookie_pwd.setMaxAge(0);
 		}
-		response.addCookie(cookie_autologin);	
+		response.addCookie(cookie_autologin);
 		response.addCookie(cookie_pwd);
 	}
-	
+
 	public String testMD5(String str) {
 		String MD5 = "";
 		try {
-
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			md.update(str.getBytes());
 			byte byteData[] = md.digest();
@@ -167,10 +173,18 @@ public class LoginController {
 			MD5 = sb.toString();
 
 		} catch (NoSuchAlgorithmException e) {
-
 			e.printStackTrace();
 			MD5 = null;
 		}
 		return MD5;
+	}
+
+	// 테스트용 가짜 유저 데이터 생성 메서드
+	private User createTestUser() {
+		User user = new User();
+		user.setUuid("testuser");
+		user.setGname("Test Group");
+		user.setUlevel(String.valueOf(1)); // 테스트 유저의 권한 레벨 설정
+		return user;
 	}
 }
